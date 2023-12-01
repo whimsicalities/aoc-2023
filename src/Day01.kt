a2480f822464cb4dd8ad135fb03c0c4a841b9dbe
whimsicalities <whimsicalities.code@gmail.com>
2023-12-01 09:33:46 +0000

    Day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,69 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var total = 0
+        for (line in input) {
+            val onlyNumericRegex = Regex("[^0-9]")
+            val strippedLine = onlyNumericRegex.replace(line, "")
+            val firstCharacter = strippedLine[0]
+            val lastCharacter = strippedLine[strippedLine.length-1]
+            val lineValue = "$firstCharacter$lastCharacter"
+            total += lineValue.toInt()
+        }
+        return total
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val wordsToNumbers = mapOf(
+                "one" to 1,
+                "1" to 1,
+                "two" to 2,
+                "2" to 2,
+                "three" to 3,
+                "3" to 3,
+                "four" to 4,
+                "4" to 4,
+                "five" to 5,
+                "5" to 5,
+                "six" to 6,
+                "6" to 6,
+                "seven" to 7,
+                "7" to 7,
+                "eight" to 8,
+                "8" to 8,
+                "nine" to 9,
+                "9" to 9,
+        )
+        var total = 0
+        for (line in input) {
+            var firstNumber = 0
+            var firstNumberIndex = Int.MAX_VALUE
+            var lastNumber = 0
+            var lastNumberIndex = -1
+            for (possibleNumbers in wordsToNumbers.entries) {
+                var currentIndex = -1
+                do {
+                    currentIndex = line.indexOf(possibleNumbers.key, currentIndex+1)
+                    if (currentIndex != -1) {
+                        if (currentIndex<firstNumberIndex) {
+                            firstNumberIndex = currentIndex
+                            firstNumber = possibleNumbers.value
+                        }
+                        if (currentIndex>lastNumberIndex) {
+                            lastNumberIndex = currentIndex
+                            lastNumber = possibleNumbers.value
+                        }
+                    }
+                } while (currentIndex != -1)
+            }
+            total += "$firstNumber$lastNumber".toInt()
+        }
+        return total
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("Day01_test_part1")
+    check(part1(testInput1) == 142)
+    val testInput2 = readInput("Day01_test_part2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
